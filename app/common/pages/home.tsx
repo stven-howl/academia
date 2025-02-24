d838c856f3163a75e8b05a66948408fea356591d
stven-howl <fbtmdgus0000@gmail.com>
2025-02-24 23:36:21 +0900

    Update home.tsx

--- app/common/pages/home.tsx
+++ app/common/pages/home.tsx
@@ -3,3 +3,11 @@ import { redirect } from "react-router";
 export function loader() {
   return redirect(`/trend_charts`);
 }
+
+export function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
